bc963740efc43221149b23c8d7d2000035fc5756
Mackenzie <mackbaird@outlook.com>
2024-05-25 22:47:00 +0100

    Functioning Haiku AI Bot
    -Edited JavaScript to ensure Typewriter plugin works.
    -Added AI API key and URL, allowing users to generate a Haiku based on what they type into prompt box

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
+function displayPoem(response) {
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+  });
+}
+
 function generatePoem(event) {
   event.preventDefault();
-  new Typewriter("#poem", {
-    strings: ["Hello", "World"],
-    autostart: true,
-    delay: 1,
-  });
+  let haikuPromptInput = document.querySelector("#haiku-prompt");
+
+  let apiKey = "a1baf73doab5c6t69019f5ca71e4d0af";
+  let prompt = `User instructions are: Generate a Haiku in English about ${haikuPromptInput.value}`;
+  let context =
+    "You have studied Japanese Haikus extensively and are a deeply philosophical Haiku expert with a love to write Haikus that fit the traditional Haiku syllable format in basic HTML and separate each line with a <br />. Make sure to follow user instructions";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
